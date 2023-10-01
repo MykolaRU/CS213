9b1d5022c98400e4b138c12fcd459b4e1115062c
Kriptopoliss <ethandivine@gmail.com>
2023-10-01 18:16:44 -0400

    Added a testbed Main, fixed bugs, everything looks like its working now

--- Project1_Methodology/src/EventCalendar.java
+++ Project1_Methodology/src/EventCalendar.java
@@ -4,6 +4,54 @@ public class EventCalendar {
     public static final int GROW_AMOUNT = 4;
     public static final int NOT_FOUND = -1;
 
+    public static void main(String args[]){ //just for testing right now
+        Date d1 = new Date("12/22/2023");
+        Timeslot t1 = Timeslot.AFTERNOON;
+        Location l1 = Location.HLL114;
+        Department de1 = Department.CS;
+        Contact c1 = new Contact(de1,"exd1@gmail.com");
+        int du1 = 30;
+        Event e1 = new Event(d1,t1,l1,c1,du1);
+
+        Date d2 = new Date("11/14/2021");
+        Timeslot t2 = Timeslot.MORNING;
+        Location l2 = Location.AB2225;
+        Department de2 = Department.BAIT;
+        Contact c2 = new Contact(de2,"exd1@gmail.com");
+        int du2 = 40;
+        Event e2 = new Event(d2,t2,l2,c2,du2);
+
+        Date d3 = new Date("11/13/2021");
+        Timeslot t3 = Timeslot.AFTERNOON;
+        Location l3 = Location.BE_AUD;
+        Department de3 = Department.ITI;
+        Contact c3 = new Contact(de3,"exd1@gmail.com");
+        int du3 = 25;
+        Event e3 = new Event(d3,t3,l3,c3,du3);
+
+        EventCalendar eventlist = new EventCalendar();
+
+        eventlist.add(e1);
+        eventlist.add(e2);
+        eventlist.add(e3);
+
+
+        eventlist.printByCampus();
+
+        eventlist.remove(e3);
+
+        eventlist.printByCampus();
+
+    }
+
+    public EventCalendar(){
+        this.events = new Event[GROW_AMOUNT];
+        this.numEvents = 0;
+    }
+
+    public Event[] getEvents(){
+        return this.events;
+    }
     private int find(Event event) {
 
         for(int i = 0; i<events.length; i++){
@@ -24,12 +72,9 @@ public class EventCalendar {
         if(numEvents == events.length)
             grow();
 
-        for(int i = (events.length - 1); i>0; i--){
-            if(events[i] != null){
-                events[i+1] = event;
-                return true;
-            }
-        }
+        events[numEvents] = event;
+        numEvents++;
+
         return false;
     }
     public boolean remove(Event event) {
@@ -41,20 +86,22 @@ public class EventCalendar {
 
         events[events.length - 1] = null;
 
+        numEvents--;
+
         return true;
     }
     public boolean contains(Event event) {
         return find(event) != -1;
     }
     public void print() {
-        for(int i=0; i< events.length; i++){
+        for (int i = 0; i<numEvents; i++){
             System.out.println(events[i]);
         }
     } //print the array as is
     public void printByDate() {
         Event holder = null;
-        for(int i = 0; i<events.length; i++){
-            for(int j = 0; j<events.length-i-1; j++){
+        for(int i = 0; i<numEvents; i++){
+            for(int j = 0; j<numEvents-i-1; j++){
                 if(events[j].getDate().compareTo(events[j+1].getDate()) == 1 ||
                 ((events[j].getDate().compareTo(events[j+1].getDate()) == 0) &&
                 (events[j].getStartTime().ConvertToMinutes() > events[j+1].getStartTime().ConvertToMinutes()))) {
@@ -64,11 +111,16 @@ public class EventCalendar {
                 }
             }
         }
+
+        for (int i = 0; i<numEvents; i++){
+                System.out.println(events[i]);
+        }
+
     } //ordered by date and timeslot
     public void printByCampus() {
         Event holder = null;
-        for(int i = 0; i<events.length; i++){
-            for(int j = 0; j<events.length-i-1; j++){
+        for(int i = 0; i<numEvents; i++){
+            for(int j = 0; j<numEvents-i-1; j++){
                 if(events[j].getLocation().getCampus().compareTo(events[j+1].getLocation().getCampus()) == 1 ||
                 ((events[j].getLocation().getCampus().compareTo(events[j+1].getLocation().getCampus()) == 0) &&
                 (events[j].getLocation().getBuildingName().compareTo(events[j+1].getLocation().getBuildingName())==1))) {
@@ -78,11 +130,15 @@ public class EventCalendar {
                 }
             }
         }
+
+        for (int i = 0; i<numEvents; i++){
+            System.out.println(events[i]);
+        }
     } //ordered by campus and building/room
     public void printByDepartment(){
         Event holder = null;
-        for(int i = 0; i<events.length; i++){
-            for(int j = 0; j<events.length-i-1; j++){
+        for(int i = 0; i<numEvents; i++){
+            for(int j = 0; j<numEvents-i-1; j++){
                 if(events[j].getContact().getDepartment().getDepartmentName().compareTo(events[j+1].getContact().getDepartment().getDepartmentName()) == 1 ||
                 ((events[j].getContact().getDepartment().getDepartmentName().compareTo(events[j+1].getContact().getDepartment().getDepartmentName()) == 0) &&
                 (events[j].getContact().getDepartment().getDepartmentName().compareTo(events[j+1].getContact().getDepartment().getDepartmentName())==1))) {
@@ -91,6 +147,10 @@ public class EventCalendar {
                     events[j + 1] = holder;
                 }
             }
+        }
+
+        for (int i = 0; i<numEvents; i++){
+            System.out.println(events[i]);
         }
     } //ordered by department done
 }
